3f2ab2caa336286f7adebd050d567acecd763fb7
CyberBishop <alabialvin@gmail.com>
2022-11-28 07:56:33 +0100

    update 6 files

--- attend.php
+++ attend.php
@@ -1,21 +1,21 @@
 <?php
 // Initialize the session
 session_start();
- 
+
 // Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: login.php");
-    exit;
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+  header("location: login.php");
+  exit;
 }
 
 //
 $now = time(); // Checking the time now when home page starts.
 
-  if ($now > $_SESSION['expire']) {
-      session_destroy();
-      header("location: login.php");
-  }
-  else { }//Starting this else one [else1]
+if ($now > $_SESSION['expire']) {
+  session_destroy();
+  header("location: login.php");
+} else {
+} //Starting this else one [else1]
 
 // database connection
 include('config.php');
@@ -24,188 +24,204 @@ $added = false;
 
 // select user details
 $sql = "SELECT * FROM student_data WHERE username = '" . $_SESSION['username'] . "'";
-    $result = mysqli_query($con,$sql);
-    $row = mysqli_fetch_array($result);
+$result = mysqli_query($con, $sql);
+$row = mysqli_fetch_array($result);
 
 
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
+
+<head>
     <meta charset="UTF-8">
     <title> Work study Portal</title>
-    
+
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-      <link rel="stylesheet" href="side.css">
-      <link rel="stylesheet"  href="button.css">
-   </head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="side.css">
+    <link rel="stylesheet" href="button.css">
+</head>
+
 <body>
-  <div class="sidebar">
-    <div class="logo-details">
-      <i class='bx bxl-c-plus-plus icon'></i>
-        <div class="logo_name">Work Study</div>
-        <i class='bx bx-menu' id="btn" ></i>
-    </div>
-    <ul class="nav-list">
-      <li>
-        <a href="home.php">
-          <i class='bx bx-grid-alt'></i>
-          <span class="links_name">Dashboard</span>
-        </a>
-         <span class="tooltip">Dashboard</span>
-      </li>
-     <li>
-       <a href="activity.php">
-         <i class='bx bxs-calendar' ></i>
-         <span class="links_name">Activity</span>
-       </a>
-       <span class="tooltip">Activity</span>
-     </li>
-     <li>
-       <a href="attendance.php">
-         <i class='bx bx-pencil' ></i>
-         <span class="links_name">Attendance</span>
-       </a>
-       <span class="tooltip">Attendance</span>
-     </li>
-     <li>
-       <a href="settings.php">
-         <i class='bx bx-cog' ></i>
-         <span class="links_name">Setting</span>
-       </a>
-       <span class="tooltip">Setting</span>
-     </li>
-     <li class="profile">
-         <div class="profile-details">
-           <img src="profile.jpg" >
-           <div class="name_job">
-             <div class="name"><?php echo  $row['Other_Name']; ?> </div>
-             <div class="job"><?php echo $row['Level']." "."Level"; ?></div>
-           </div>
-         </div>
-        <a href="logout.php" ><i class='bx bx-log-out' id="log_out"></i></a>
-     </li>
-    </ul>
-  </div>
-  
- <section>
-
-  <div class="cards" style="width: 18rem;">
-        <div class="cards-body">
-          <h3 class="card-title">Attendance Card</h3>
-          <p class="card-text">Please make sure to sign in and sign out properly. Any issues should be reported to Seald Office</p>
-          
-          <form action = "attendance.php" id="loginFrm" method="POST">
-
-          <button class="bite " type="submit" role="button" name="signinbutton" type="submit">SIGN IN</button>
-          <button class="bite " type="submit" role="button" name="signoutbutton" type="submit">SIGN OUT</button>
-
-
-
-          <?php
-        if (isset($_POST['signinbutton'])){    
-        $student = $_SESSION['id'];
-        $student_name = $row['Last_Name']. " ".$row['Other_Name'];
-        
-        $timein = date("H:i", strtotime("+1 HOURS"));
-        // $timein = time.now();
-        $date = date("Y-m-d", strtotime("+1 HOURS"));
-        
-        $con->query("INSERT INTO `time_table` VALUES('', '$student', '$student_name', '$timein', '$timeout', '$total_time', '$date')") or die(mysqli_error());
-        echo "<p class = 'para'>"."Signed in "." <label class = ''>at  ".date("h:i a", strtotime($timein))."</label></p>"; 
-            }
-        ?>
-        <?php
-        if (isset($_POST['signoutbutton'])){    
-        $student = $_SESSION['id'];
-        $timeout = date("H:i", strtotime("+1 HOURS"));
-        // $timeout = time();
-
-        $date = date("Y-m-d", strtotime("+1 HOURS"));
-        $student_name = $row['Last_Name']. " ".$row['Other_Name'];
-
-        $last = "SELECT MAX(time_id) AS last_id FROM time_table WHERE student_no = '" . $_SESSION['id'] . "'";
-        $result = mysqli_query($con, $last);
-        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-        $last_id = $row['last_id'];
-
-        $sessid = $_SESSION['id'];
-
-        $keep = $con->query("SELECT `timein` FROM time_table WHERE student_no = $sessid ORDER BY time_id DESC") or die(mysqli_error());
-        $pull = mysqli_fetch_array($keep, MYSQLI_ASSOC);
-        $timein = strtotime($pull['timein']);
-        $calctimeout = strtotime($timeout);
-
-        $workingHours = ($calctimeout-$timein)/3600;
-        $con->query("UPDATE `time_table` SET `timeout` = '$timeout' WHERE time_id = '$last_id'") or die(mysqli_error());
-        $con->query("UPDATE `time_table` SET `total_time` = '$workingHours' WHERE time_id = '$last_id'") or die(mysqli_error());
-
-        echo "<p class = 'para'>"."Signed out "." <label class = ''>at  ".date("h:i a", strtotime($timeout))."</label></p>"; 
-        // echo $workingHours;
-        // date('Y-m-d H:i:s', strtotime($discount_start_date));
-      }
-?>
-
-
-          </div>
-    </div>
-    <div class="main">
-        
-        <div class="loader">
-          <div class="check">
-            <span class="check-one"></span>
-            <span class="check-two"></span>
-          </div>
+    <div class="sidebar">
+        <div class="logo-details">
+            <i class='bx bxl-c-plus-plus icon'></i>
+            <div class="logo_name">Work Study</div>
+            <i class='bx bx-menu' id="btn"></i>
         </div>
-        
-      </div>
-      </section>
+        <ul class="nav-list">
+            <li>
+                <a href="home.php">
+                    <i class='bx bx-grid-alt'></i>
+                    <span class="links_name">Dashboard</span>
+                </a>
+                <span class="tooltip">Dashboard</span>
+            </li>
+            <li>
+                <a href="activity.php">
+                    <i class='bx bxs-calendar'></i>
+                    <span class="links_name">Activity</span>
+                </a>
+                <span class="tooltip">Activity</span>
+            </li>
+            <li>
+                <a href="attendance.php">
+                    <i class='bx bx-pencil'></i>
+                    <span class="links_name">Attendance</span>
+                </a>
+                <span class="tooltip">Attendance</span>
+            </li>
+            <li>
+                <a href="settings.php">
+                    <i class='bx bx-cog'></i>
+                    <span class="links_name">Setting</span>
+                </a>
+                <span class="tooltip">Setting</span>
+            </li>
+            <li class="profile">
+                <div class="profile-details">
+                    <img src="profile.jpg">
+                    <div class="name_job">
+                        <div class="name">
+                            <?php echo $row['Other_Name']; ?>
+                        </div>
+                        <div class="job">
+                            <?php echo $row['Level'] . " " . "Level"; ?>
+                        </div>
+                    </div>
+                </div>
+                <a href="logout.php"><i class='bx bx-log-out' id="log_out"></i></a>
+            </li>
+        </ul>
+    </div>
+
+    <section>
+
+        <div class="cards" style="width: 18rem;">
+            <div class="cards-body">
+                <h3 class="card-title">Attendance Card</h3>
+                <p class="card-text">Please make sure to sign in and sign out properly. Any issues should be reported to
+                    Seald
+                    Office</p>
+
+                <form action="attendance.php" id="loginFrm" method="POST">
+
+                    <button class="bite " type="submit" role="button" name="signinbutton" type="submit">SIGN IN</button>
+                    <button class="bite " type="submit" role="button" name="signoutbutton" type="submit">SIGN
+                        OUT</button>
 
 
 
-<script>
-        let sidebar = document.querySelector(".sidebar");
-        let closeBtn = document.querySelector("#btn");
-        let searchBtn = document.querySelector(".bx-search");
+                    <?php
+                    if (isset($_POST['signinbutton'])) {
+                      $student = $_SESSION['id'];
+                      $student_name = $row['Last_Name'] . " " . $row['Other_Name'];
 
-        closeBtn.addEventListener("click", ()=>{
-            sidebar.classList.toggle("open");
-            menuBtnChange();//calling the function(optional)
-        });
+                      $timein = date("H:i", strtotime("+1 HOURS"));
+                      // $timein = time.now();
+                      $date = date("Y-m-d", strtotime("+1 HOURS"));
 
-        searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search icon
-            sidebar.classList.toggle("open");
-            menuBtnChange(); //calling the function(optional)
-        });
+                      $con->query("INSERT INTO `time_table` VALUES('', '$student', '$student_name', '$timein', '$timeout', '$total_time', '$date')") or die(mysqli_error());
+                      echo "<p class = 'para'>" . "Signed in " . " <label class = ''>at  " . date("h:i a", strtotime($timein)) . "</label></p>";
+                    }
+                    ?>
+                    <?php
+                    if (isset($_POST['signoutbutton'])) {
+                      $student = $_SESSION['id'];
+                      $timeout = date("H:i", strtotime("+1 HOURS"));
+                      // $timeout = time();
+                    
+                      $date = date("Y-m-d", strtotime("+1 HOURS"));
+                      $student_name = $row['Last_Name'] . " " . $row['Other_Name'];
 
-        // following are the code to change sidebar button(optional)
-        function menuBtnChange() {
-        if(sidebar.classList.contains("open")){
-            closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the icons class
-        }else {
-            closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the icons class
+                      $last = "SELECT MAX(time_id) AS last_id FROM time_table WHERE student_no = '" . $_SESSION['id'] . "'";
+                      $result = mysqli_query($con, $last);
+                      $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                      $last_id = $row['last_id'];
+
+                      $sessid = $_SESSION['id'];
+
+                      $keep = $con->query("SELECT `timein` FROM time_table WHERE student_no = $sessid ORDER BY time_id DESC") or die(mysqli_error());
+                      $pull = mysqli_fetch_array($keep, MYSQLI_ASSOC);
+                      $timein = strtotime($pull['timein']);
+                      $calctimeout = strtotime($timeout);
+
+                      $workingHours = ($calctimeout - $timein) / 3600;
+                      $con->query("UPDATE `time_table` SET `timeout` = '$timeout' WHERE time_id = '$last_id'") or die(mysqli_error());
+                      $con->query("UPDATE `time_table` SET `total_time` = '$workingHours' WHERE time_id = '$last_id'") or die(mysqli_error());
+
+                      echo "<p class = 'para'>" . "Signed out " . " <label class = ''>at  " . date("h:i a", strtotime($timeout)) . "</label></p>";
+                      // echo $workingHours;
+                      // date('Y-m-d H:i:s', strtotime($discount_start_date));
+                    }
+                    ?>
+
+
+            </div>
+        </div>
+        <div class="main">
+
+            <div class="loader">
+                <div class="check">
+                    <span class="check-one"></span>
+                    <span class="check-two"></span>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+
+
+    <script>
+    let sidebar = document.querySelector(".sidebar");
+    let closeBtn = document.querySelector("#btn");
+    let searchBtn = document.querySelector(".bx-search");
+
+    closeBtn.addEventListener("click", () => {
+        sidebar.classList.toggle("open");
+        menuBtnChange(); //calling the function(optional)
+    });
+
+    searchBtn.addEventListener("click", () => { // Sidebar open when you click on the search icon
+        sidebar.classList.toggle("open");
+        menuBtnChange(); //calling the function(optional)
+    });
+
+    // following are the code to change sidebar button(optional)
+    function menuBtnChange() {
+        if (sidebar.classList.contains("open")) {
+            closeBtn.classList.replace("bx-menu", "bx-menu-alt-right"); //replacing the icons class
+        } else {
+            closeBtn.classList.replace("bx-menu-alt-right", "bx-menu"); //replacing the icons class
         }
-        }
+    }
 
-        document.addEventListener('DOMContentLoaded', function () {
+    document.addEventListener('DOMContentLoaded', function() {
         var btn = document.querySelector('.bite'),
-        loader = document.querySelector('.loader'),
-        check = document.querySelector('.check');
-  
-        btn.addEventListener('click', function () {
-            loader.classList.add('active');    
-        });
-        
-        loader.addEventListener('animationend', function() {
-            check.classList.add('active'); 
-        });
+            loader = document.querySelector('.loader'),
+            check = document.querySelector('.check');
+
+        btn.addEventListener('click', function() {
+            loader.classList.add('active');
         });
 
-  </script>
+        loader.addEventListener('animationend', function() {
+            check.classList.add('active');
+        });
+    });
+    </script>
 </body>
+
 </html>
--- login.php
+++ login.php
@@ -1,63 +1,63 @@
 <?php
 // Initialize the session
 session_start();
- 
+
 // Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-  header("location: home.php");
-  exit;
+if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+    header("location: home.php");
+    exit;
 }
- 
+
 // Include config file
 // require_once "config.php";
- 
+
 // Define variables and initialize with empty values
 $username = $password = "";
 $username_err = $password_err = "";
- 
+
 // Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
     // Check if username is empty
-    if(empty(trim($_POST["username"]))){
+    if (empty(trim($_POST["username"]))) {
         $username_err = "Please enter username.";
-    } else{
+    } else {
         $username = trim($_POST["username"]);
     }
-    
+
     // Check if password is empty
-    if(empty(trim($_POST["password"]))){
+    if (empty(trim($_POST["password"]))) {
         $password_err = "Please enter your password.";
-    } else{
+    } else {
         $password = trim($_POST["password"]);
     }
-    
+
     // Validate credentials
-    if(empty($username_err) && empty($password_err)){
+    if (empty($username_err) && empty($password_err)) {
         // Prepare a select statement
         $sql = "SELECT id, username, password FROM student_data WHERE username = ?";
-        
-        if($stmt = mysqli_prepare($con, $sql)){
+
+        if ($stmt = mysqli_prepare($con, $sql)) {
             // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "s", $param_username);
-            
+
             // Set parameters
             $param_username = $username;
-            
+
             // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
+            if (mysqli_stmt_execute($stmt)) {
                 // Store result
                 mysqli_stmt_store_result($stmt);
-                
+
                 // Check if username exists, if yes then verify password
-                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                if (mysqli_stmt_num_rows($stmt) == 1) {
                     // Bind result variables
                     mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
-                    if(mysqli_stmt_fetch($stmt)){
-                        if(password_verify($password, $hashed_password)){
+                    if (mysqli_stmt_fetch($stmt)) {
+                        if (password_verify($password, $hashed_password)) {
                             // Password is correct, so start a new session
                             session_start();
-                            
+
                             // Store data in session variables
                             $_SESSION["loggedin"] = true;
                             $_SESSION["id"] = $id;
@@ -73,27 +73,27 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             // $_SESSION["Program"] = $Program;
                             // $_SESSION["Level"] = $Level;
                             // $_SESSION["username"] = $Placement;                            
-                            
+
                             // Redirect user to welcome page
                             header("location: home.php");
-                        } else{
+                        } else {
                             // Display an error message if password is not valid
                             $password_err = "The password you entered was not valid.";
                         }
                     }
-                } else{
+                } else {
                     // Display an error message if username doesn't exist
                     $username_err = "No account found with that username.";
                 }
-            } else{
+            } else {
                 echo "Oops! Something went wrong. Please try again later.";
             }
         }
-        
+
         // Close statement
         mysqli_stmt_close($stmt);
     }
-    
+
     // Close connection
     mysqli_close($con);
 }
@@ -167,7 +167,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                                     <label for="email">Matric No.</label>
                                     <input type="text" name="username" class="form-control"
                                         value="<?php echo $username; ?>">
-                                    <span class="help-block"><?php echo $username_err; ?></span>
+                                    <span class="help-block">
+                                        <?php echo $username_err; ?>
+                                    </span>
                                 </div>
                                 <div
                                     class="field padding-bottom--48 form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
