2a113f34dfe0c1ad0b96df548e11f32ec07de249
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2022-11-28 07:47:12 +0100

    update settings.php

--- settings.php
+++ settings.php
@@ -19,6 +19,64 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
     // echo "Hello, " . $row['Last_Name'] . " " . $row['Email'] . " ";
 
+    // Define variables and initialize with empty values
+$new_password = $confirm_password = "";
+$new_password_err = $confirm_password_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Validate new password
+    if(empty(trim($_POST["new_password"]))){
+        $new_password_err = "Please enter the new password.";     
+    } elseif(strlen(trim($_POST["new_password"])) < 6){
+        $new_password_err = "Password must have at least 6 characters.";
+    } else{
+        $new_password = trim($_POST["new_password"]);
+    }
+    
+    // Validate confirm password
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Please confirm the password.";
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($new_password_err) && ($new_password != $confirm_password)){
+            $confirm_password_err = "Password did not match.";
+        }
+    }
+        
+    // Check input errors before updating the database
+    if(empty($new_password_err) && empty($confirm_password_err)){
+        // Prepare an update statement
+        $sql = "UPDATE users SET password = ? WHERE id = ?";
+        
+        if($stmt = mysqli_prepare($con, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "si", $param_password, $param_id);
+            
+            // Set parameters
+            $param_password = password_hash($new_password, PASSWORD_DEFAULT);
+            $param_id = $_SESSION["id"];
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Password updated successfully. Destroy the session, and redirect to login page
+                session_destroy();
+                header("location: login.php");
+                exit();
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+        
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($con);
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -114,9 +172,6 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
         </p>
     </section>
 
-    <!-- Button trigger modal -->
-
-    <!-- Modal -->
     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
         <div class="modal-dialog border-0" role="document" style="width: 25rem;">
@@ -127,8 +182,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-
-                <div class="card mx-auto p-4 shadow-none" style="width: 24rem;"><br>
+                <div class="card mx-auto p-4 shadow-none" style="width: 24rem;">
                     <div class="card-body shadow-none">
                         <p>Please fill out this form to reset your password.</p>
                         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
@@ -144,7 +198,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
                                 <span class="help-block"><?php echo $confirm_password_err; ?></span>
                             </div>
                             <div class="form-group">
-                                <input type="submit" class="btn btn-primary w-100 mb-3" value="Submit"
+                                <input type="submit" class="btn btn-primary w-100 mt-4 mb-2" value="Submit"
                                     data-dismiss="modal">
                                 <br>
                                 <a class="btn btn-danger w-100" href="home.php" data-dismiss="modal">Cancel</a>
