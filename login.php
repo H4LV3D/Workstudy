c8684b07522e64e1ae9ac0d7143e7142fa63e8b8
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2022-11-29 06:08:51 +0100

    update login.css, profile.jpg, home.php and login.php

--- login.php
+++ login.php
@@ -104,6 +104,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html>
 
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charset="utf-8">
     <title>Workstudy - work and earn as students</title>
     <link rel="stylesheet" type="text/css" href="/assets/css/login.css">
