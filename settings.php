674be24aea3a069e88d304c08229996adf002d98
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-03-09 19:47:34 +0100

    modified 4 files in .

--- settings.php
+++ settings.php
@@ -1,86 +1,86 @@
 <?php
-    // Initialize the session
-    session_start();
-    
-    // Check if the user is logged in, otherwise redirect to login page
-    if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-        header("location: login.php");
-        exit;
+// Initialize the session
+session_start();
+
+// Check if the user is logged in, otherwise redirect to login page
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+    header("location: login.php");
+    exit;
+}
+
+require_once "config.php";
+
+// Define variables and initialize with empty values
+$new_password = $confirm_password = "";
+$new_password_err = $confirm_password_err = "";
+
+// Processing form data when form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Validate new password
+    if (empty(trim($_POST["new_password"]))) {
+        $new_password_err = "Please enter the new password.";
+    } elseif (strlen(trim($_POST["new_password"])) < 8) {
+        $new_password_err = "Password must have at least 8 characters.";
+    } else {
+        $new_password = trim($_POST["new_password"]);
     }
-    
-    require_once "config.php";
-    
-    // Define variables and initialize with empty values
-    $new_password = $confirm_password = "";
-    $new_password_err = $confirm_password_err = "";
 
-    // Processing form data when form is submitted
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
+    if ($new_password_err) {
+        echo "<div id='error' style='color:red;'>" . $new_password_err . "</div>";
+    }
 
-        // Validate new password
-        if(empty(trim($_POST["new_password"]))){
-            $new_password_err = "Please enter the new password.";     
-        } elseif(strlen(trim($_POST["new_password"])) < 8) {
-            $new_password_err = "Password must have at least 8 characters.";
-        } else{
-            $new_password = trim($_POST["new_password"]);
+    // Validate confirm password
+    if (empty(trim($_POST["confirm_password"]))) {
+        $confirm_password_err = "Please confirm the password.";
+    } else {
+        $confirm_password = trim($_POST["confirm_password"]);
+        if (empty($new_password_err) && ($new_password != $confirm_password)) {
+            $confirm_password_err = "Passwords did not match.";
         }
+    }
 
-        if($new_password_err){
-            echo "<div id='error' style='color:red;'>" . $new_password_err . "</div>";
-        }
+    if ($confirm_password_err) {
+        echo "<div id='error' style='color:red;'>" . $confirm_password_err . "</div>";
+    }
 
-        // Validate confirm password
-        if(empty(trim($_POST["confirm_password"]))){
-            $confirm_password_err = "Please confirm the password.";
-        } else{
-            $confirm_password = trim($_POST["confirm_password"]);
-            if(empty($new_password_err) && ($new_password != $confirm_password)){
-                $confirm_password_err = "Passwords did not match.";
+    // Check input errors before updating the database
+    if (empty($new_password_err) && empty($confirm_password_err)) {
+        // Prepare an update statement
+        $sql = "UPDATE student_data SET password = ? WHERE id = ?";
+
+        if ($stmt = mysqli_prepare($con, $sql)) {
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "si", $param_password, $param_id);
+
+            // Set parameters
+            $param_password = password_hash($new_password, PASSWORD_DEFAULT);
+            $param_id = $_SESSION["id"];
+
+            // Attempt to execute the prepared statement
+            if (mysqli_stmt_execute($stmt)) {
+                // Password updated successfully. Destroy the session, and redirect to login page
+                session_destroy();
+                header("location: login.php");
+                exit();
+            } else {
+                echo "Oops! Something went wrong. Please try again later.";
             }
         }
 
-        if($confirm_password_err){
-            echo "<div id='error' style='color:red;'>" . $confirm_password_err . "</div>";
-        }
-
-        // Check input errors before updating the database
-        if(empty($new_password_err) && empty($confirm_password_err)){
-            // Prepare an update statement
-            $sql = "UPDATE student_data SET password = ? WHERE id = ?";
-            
-            if($stmt = mysqli_prepare($con, $sql)){
-                // Bind variables to the prepared statement as parameters
-                mysqli_stmt_bind_param($stmt, "si", $param_password, $param_id);
-                
-                // Set parameters
-                $param_password = password_hash($new_password, PASSWORD_DEFAULT);
-                $param_id = $_SESSION["id"];
-                
-                // Attempt to execute the prepared statement
-                if(mysqli_stmt_execute($stmt)){
-                    // Password updated successfully. Destroy the session, and redirect to login page
-                    session_destroy();
-                    header("location: login.php");
-                    exit();
-                } else{
-                    echo "Oops! Something went wrong. Please try again later.";
-                }
-            }
-            
-            // Close statement
-            // mysqli_stmt_close($stmt);
-        }
-
-        // Close connection
-        mysqli_close($con);
+        // Close statement
+        // mysqli_stmt_close($stmt);
     }
+
+    // Close connection
+    mysqli_close($con);
+}
 ?>
 
 <?php
-    // $sql = "SELECT * FROM student_data WHERE username = '" . $_SESSION['username'] . "'";
-    // $result = mysqli_query($con, $sql);
-    // $row = mysqli_fetch_array($result);
+// $sql = "SELECT * FROM student_data WHERE username = '" . $_SESSION['username'] . "'";
+// $result = mysqli_query($con, $sql);
+// $row = mysqli_fetch_array($result);
 ?>
 
 <!DOCTYPE html>
@@ -91,21 +91,17 @@
     <title> Work study Portal</title>
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
     </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
     </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
     </script>
-    <link rel="stylesheet" href="./assets/css/side.css">
-    <link href="./assets/fontawesome-free-6.2.1-web/css/fontawesome.css" rel="stylesheet">
-    <link href="./assets/fontawesome-free-6.2.1-web/css/brands.css" rel="stylesheet">
-    <link href="./assets/fontawesome-free-6.2.1-web/css/solid.css" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/css/side.css">
+    <link href="/assets/fontawesome-free-6.2.1-web/css/fontawesome.css" rel="stylesheet">
+    <link href="/assets/fontawesome-free-6.2.1-web/css/brands.css" rel="stylesheet">
+    <link href="/assets/fontawesome-free-6.2.1-web/css/solid.css" rel="stylesheet">
 </head>
 
 <body>
@@ -160,8 +156,7 @@
 
     <div class="d-md-none fixed-bottom mb-2">
         <div class="row d-flex justify-content-center items-align-center">
-            <ul
-                class="col-10 d-flex flex-row justify-content-between align-items-center my-auto py-3 px-5 border rounded-15 shadow bg-white">
+            <ul class="col-10 d-flex flex-row justify-content-between align-items-center my-auto py-3 px-5 border rounded-15 shadow bg-white">
                 <a href="index.php" class="text-decoration-none text-light">
                     <i class='bx bx-grid-alt' style="color:#000;"></i>
                 </a>
@@ -192,8 +187,7 @@
                     </h4>
                 </div>
                 <hr>
-                <button type="button" class="btn btn-warning shadow-none border-0 text-white px-5 px-sm-4 py-2"
-                    style="background-color: #996399;" data-toggle="modal" data-target="#exampleModal">
+                <button type="button" class="btn btn-warning shadow-none border-0 text-white px-5 px-sm-4 py-2" style="background-color: #996399;" data-toggle="modal" data-target="#exampleModal">
                     Reset Your Password
                 </button>
                 <div class="d-md-none">
@@ -210,8 +204,7 @@
         </div>
     </section>
 
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog border-0" role="document" style="width: 25rem;">
             <div class="modal-content">
                 <div class="modal-header">
@@ -226,8 +219,7 @@
                         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                             <div class="form-group <?php echo (!empty($new_password_err)) ? 'has-error' : ''; ?>">
                                 <label>New Password</label>
-                                <input type="password" name="new_password" class="form-control"
-                                    value="<?php echo $new_password; ?>">
+                                <input type="password" name="new_password" class="form-control" value="<?php echo $new_password; ?>">
                                 <span class="help-block"><?php echo $new_password_err; ?></span>
                             </div>
                             <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
@@ -250,28 +242,28 @@
 
     <script src="//cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
     <script>
-    $(document).ready(function() {
-        $('#myTable').DataTable();
-    });
+        $(document).ready(function() {
+            $('#myTable').DataTable();
+        });
     </script>
     <script>
-    let sidebar = document.querySelector(".sidebar");
-    let closeBtn = document.querySelector("#btn");
-    let searchBtn = document.querySelector(".bx-search");
-    let button = document.querySelector(".bx-menu");
-    button.addEventListener("click", () => {
-        // sidebar.classList.toggle("open");
-        searchBtn.classList.toggle("bx-search");
-        searchBtn.classList.toggle("bx-menu");
-    });
+        let sidebar = document.querySelector(".sidebar");
+        let closeBtn = document.querySelector("#btn");
+        let searchBtn = document.querySelector(".bx-search");
+        let button = document.querySelector(".bx-menu");
+        button.addEventListener("click", () => {
+            // sidebar.classList.toggle("open");
+            searchBtn.classList.toggle("bx-search");
+            searchBtn.classList.toggle("bx-menu");
+        });
 
-    closeBtn.addEventListener("click", () => {
-        sidebar.classList.toggle("open");
-        $(button).toggleClass('bx-search bxs-x-square');
-    });
-    $(document).ready(function() {
-        $('.dropdown-toggle').dropdown()
-    });
+        closeBtn.addEventListener("click", () => {
+            sidebar.classList.toggle("open");
+            $(button).toggleClass('bx-search bxs-x-square');
+        });
+        $(document).ready(function() {
+            $('.dropdown-toggle').dropdown()
+        });
     </script>
 </body>
 
