c19bdfc5827df48a3e37bb2b2a2cbff5c9578eb5
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-03-09 19:47:34 +0100

    modified 4 files in .

--- attendance.php
+++ attendance.php
@@ -47,11 +47,11 @@ $row = mysqli_fetch_array($result);
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
         integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
     </script>
-    <link rel="stylesheet" href="./assets/css/side.css">
+    <link rel="stylesheet" href="/assets/css/side.css">
     <script src="attendance.js"></script>
-    <link href="./assets/fontawesome-free-6.2.1-web/css/fontawesome.css" rel="stylesheet">
-    <link href="./assets/fontawesome-free-6.2.1-web/css/brands.css" rel="stylesheet">
-    <link href="./assets/fontawesome-free-6.2.1-web/css/solid.css" rel="stylesheet">
+    <link href="/assets/fontawesome-free-6.2.1-web/css/fontawesome.css" rel="stylesheet">
+    <link href="/assets/fontawesome-free-6.2.1-web/css/brands.css" rel="stylesheet">
+    <link href="/assets/fontawesome-free-6.2.1-web/css/solid.css" rel="stylesheet">
 </head>
 
 <body>
