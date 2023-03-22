8855301290d7781a5dd7700077a604b1085a87fc
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-03-22 08:57:38 +0100

    modified supervisor.php in .

--- supervisor.php
+++ supervisor.php
@@ -39,18 +39,14 @@ $row = mysqli_fetch_array($result);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title> Work Study | Admin Dashboard</title>
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" type="text/css"
-        href="https://cdn.datatables.net/r/bs-3.3.5/jq-2.1.4,dt-1.10.8/datatables.min.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/r/bs-3.3.5/jq-2.1.4,dt-1.10.8/datatables.min.css" />
     <script type="text/javascript" src="https://cdn.datatables.net/r/bs-3.3.5/jqc-1.11.3,dt-1.10.8/datatables.min.js">
     </script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
-        integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous">
     </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
     </script>
     <script src="https://kit.fontawesome.com/2029614d15.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="/assets/css/side.css">
@@ -123,13 +119,14 @@ $row = mysqli_fetch_array($result);
 
         ?>
         <div class="flex flex-row justify-content-end">
-            <button class="btn" style="background-color:#996399;" type="button" data-toggle="modal"
-                data-target="#myModal">
+            <button class="btn" style="background-color:#996399;" type="button" data-toggle="modal" data-target="#myModal">
                 <a href="addstudent.php" class="fa fa-plus text-decoration-none text-white">
                     <span>New Student</span>
                 </a>
             </button>
         </div>
+
+        <h1 class="text-center">Supervisor Students Information</h1>
 
         <div class="my-5">
             <table class="table table-bordered table-striped table-hover" id="myTable">
@@ -165,7 +162,6 @@ $row = mysqli_fetch_array($result);
 						<td class='text-left'>$Email</td>
 						<td class='text-center'>$Program</td>
 						<td class='text-center'>$Level</td>
-						<td class='text-center'>$Placement</td>
 					</tr>
 					";
                 }
@@ -173,30 +169,49 @@ $row = mysqli_fetch_array($result);
             </table>
         </div>
         <form method="post" action="export.php">
-            <input type="submit" name="export" class="btn px-5 py-2" value="Export Student Record"
-                style="background-color: #996399;color: #eee;" />
+            <input type="submit" name="export" class="btn px-5 py-2" value="Export Student Record" style="background-color: #996399;color: #eee;" />
         </form>
+
+        <h1 class="text-center">Student Weekly Comment</h1>
+
+        <div class="border w-1/2">
+            <?php
+            $get_data = "SELECT * FROM student_data order by 1 desc";
+            $run_data = mysqli_query($con, $get_data);
+            $i = 0;
+            while ($row = mysqli_fetch_array($run_data)) {
+                $id = $row['comment_id'];
+                $comment = $row['comment'];
+
+                echo "
+					<p>
+                        $comment
+					</p>
+					";
+            }
+            ?>
+        </div>
     </div>
 
     <script src="//cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
     <script>
-    $(document).ready(function() {
-        $('#myTable').DataTable();
-    });
+        $(document).ready(function() {
+            $('#myTable').DataTable();
+        });
     </script>
     <script>
-    let sidebar = document.querySelector(".sidebar");
-    let closeBtn = document.querySelector("#btn");
-    let searchBtn = document.querySelector(".bx-search");
-    let button = document.querySelector(".bx-menu");
+        let sidebar = document.querySelector(".sidebar");
+        let closeBtn = document.querySelector("#btn");
+        let searchBtn = document.querySelector(".bx-search");
+        let button = document.querySelector(".bx-menu");
 
-    closeBtn.addEventListener("click", () => {
-        sidebar.classList.toggle("open");
-        $(button).toggleClass('bx-search bxs-x-square');
-    });
-    $(document).ready(function() {
-        $('.dropdown-toggle').dropdown()
-    });
+        closeBtn.addEventListener("click", () => {
+            sidebar.classList.toggle("open");
+            $(button).toggleClass('bx-search bxs-x-square');
+        });
+        $(document).ready(function() {
+            $('.dropdown-toggle').dropdown()
+        });
     </script>
 </body>
 
