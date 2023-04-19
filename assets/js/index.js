232b3ee87370f12904f5d713b90e34973e459bca
cyberbishop <alabialvin@gmail.com>
2023-04-19 16:54:12 +0100

    added token redeclaration to index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,16 @@
+function getCookie(name) {
+	const cookies = document.cookie.split(";");
+	for (let i = 0; i < cookies.length; i++) {
+		const cookie = cookies[i].trim();
+		if (cookie.startsWith(name + "=")) {
+			return decodeURIComponent(cookie.substring(name.length + 1));
+		}
+	}
+	return null;
+}
+
+token = getCookie("token");
+
 window.onload = () => {
 	// Send the user object to the server
 	fetch(`${API_URL}/users/me`, {
