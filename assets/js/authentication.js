af97938b88ac456c09b9cfbfe1ee37b32e816b56
Oluwatimilehin Alabi <alabialvin@gmail.com>
2023-04-26 23:05:38 +0100

    Majorly prettier

--- assets/js/authentication.js
+++ assets/js/authentication.js
@@ -4,42 +4,42 @@ const API_URL = "https://cu-workstudy-backend.cyclic.app";
 const token = localStorage.getItem("token");
 
 fetch(`${API_URL}/users/verify`, {
-  method: "GET",
-  headers: {
-    Authorization: `token ${token}`,
-  },
+	method: "GET",
+	headers: {
+		Authorization: `token ${token}`,
+	},
 })
-  .then((response) => response.json())
-  .then((data) => {
-    if (data.error) {
-      window.location.href = "/portal/login.html";
-    }
-    if (data.length === 0) {
-      logout();
-    }
-    checkAuthorization(data.role);
-  });
+	.then((response) => response.json())
+	.then((data) => {
+		if (data.error) {
+			window.location.href = "/portal/login.html";
+		}
+		if (data.length === 0) {
+			logout();
+		}
+		checkAuthorization(data.role);
+	});
 
 function checkAuthorization(role) {
-  if (role === "admin" && window.location.pathname.includes("/admin")) {
-    // user has admin role and is trying to access admin page
-    return;
-  } else if (
-    role === "student" &&
-    window.location.pathname.includes("/admin")
-  ) {
-    // user has student role and is trying to access admin page
-    window.location.href = "/index.html";
-  } else if (role === "admin" && !window.location.pathname.includes("/admin")) {
-    // user has admin role and is trying to access student page
-    window.location.href = "/admin-dashboard.html";
-  }
+	if (role === "admin" && window.location.pathname.includes("/admin")) {
+		// user has admin role and is trying to access admin page
+		return;
+	} else if (
+		role === "student" &&
+		window.location.pathname.includes("/admin")
+	) {
+		// user has student role and is trying to access admin page
+		window.location.href = "/index.html";
+	} else if (role === "admin" && !window.location.pathname.includes("/admin")) {
+		// user has admin role and is trying to access student page
+		window.location.href = "/admin-dashboard.html";
+	}
 }
 
 function logout() {
-  // Remove the token from local storage
-  localStorage.removeItem("token");
+	// Remove the token from local storage
+	localStorage.removeItem("token");
 
-  // Redirect to the login page
-  window.location.href = "/portal/login.html";
+	// Redirect to the login page
+	window.location.href = "/portal/login.html";
 }
